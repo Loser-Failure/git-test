a93fc70324d9a7841a57db41f0c7974c8d925a58
athdf <hdf@qq.com>
2021-07-03 11:09:30 +0800

    first commit

---
+++ src/main/java/com/athdf/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.athdf.git;
+
+/**
+ * @author hdfstart
+ * @create 2021-07-0311:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
